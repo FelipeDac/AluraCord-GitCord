91c295750d0d9cf4cef51b8212707edb645cd05d
FelipeDac <felipe3660@gmail.com>
2022-01-26 13:43:12 -0300

    adicionado globalstyle para responsividade

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,39 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
+function GlobalStyle() {
+    return (
+        <style global jsx>{`
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizin: border-box;
+            list-style: none;
+        }
+        body {
+            font-family: 'Open Sans', sans-serif;
+        }
+        /* App fit Height */
+        html, body, #__next {
+            min-height: 100vh;
+            display: flex;
+            flex: 1;
+        }
+        #__next {
+            flex: 1;
+        }
+        #__next > * {
+            flex: 1;
+        }
+        /* ./App fit Height */
+        `}</style>
+    );
+}
+
 function Titulo(props) {
     const Tag = props.tag || 'h1';
     return (
-        <>
+        <GlobalStyle>
             <Tag>{props.children}</Tag>
             <style jsx>{`
             ${Tag} {
@@ -15,7 +44,7 @@ function Titulo(props) {
                 font-weight: 600;
             }
             `}</style>
-        </>
+        </GlobalStyle>
     );
 }
 
